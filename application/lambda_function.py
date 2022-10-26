fc4b6f91f18aab393a5065910c401fd8ba355713
DC <78552552+DC@users.noreply.github.com>
2022-10-26 18:35:33 -0400

    Added filename format conversion

--- application/lambda_function.py
+++ application/lambda_function.py
@@ -17,21 +17,19 @@ def lambda_handler(event, context):
     
     clear_tmp_files()
 
-    # Get the object from the event and show its content type
+    # Get the object and object's key from the event
     bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    video_file_name = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     
-    video_file_name = key[:key.index(" ")]
-    start_timestamp = key[key.index(" ")+1:key.index(" ", -1)]
-    end_timestamp = key[key.index(" ", -1)+1:]
-    print(video_file_name)
-    print(start_timestamp)
-    print(end_timestamp)
+    # Make the file suitable for extracting timestamps in the right format
+    video_timestamps = video_file_name.replace("-", ":")
+
+    print("Video file name:", video_file_name)
+    print("Changed file format:", video_timestamps)
 
     video_path = download_vid_to_tmp(bucket, video_file_name)
     
-    trim_uploaded_video(video_path, video_file_name, bucket, start_timestamp, end_timestamp)
-
+    trim_uploaded_video(video_path, video_file_name, bucket, video_timestamps)
     
     return {
         'statusCode': 200,
