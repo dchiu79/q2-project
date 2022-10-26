7affd9dd9c80e29eb881b284243d290fde8d557f
DC <78552552+DC@users.noreply.github.com>
2022-10-26 18:37:23 -0400

    Added retrieval of start and end timestamps

--- application/video_trim.py
+++ application/video_trim.py
@@ -1,34 +1,35 @@
 import shlex
 import subprocess
-import urllib3
-import json
 
 import boto3
 
 s3 = boto3.client('s3')
 
-def trim_uploaded_video(video_path, video_file_name, bucket, start_timestamp, end_timestamp):
-    trimmed_video_name = "{}.mp4".format(video_file_name[:-4])
+def trim_uploaded_video(video_path, video_file_name, bucket, video_timestamps):
+    
+    # Creates video name and path for a trimmed version
+    trimmed_video_name = "trimmed_{}.mp4".format(video_file_name[:-4])
     trimmed_video_path = "/tmp/{}".format(trimmed_video_name)
     
-    #ffmpeg command to trim a video based on two timestamps
+    # Finds the indexes where the timestamps are split
+    first_split = video_timestamps.index("+")
+    second_split = video_timestamps.index("+", first_split+1)
+    
+    print("First \"+\" position:", first_split)
+    print("Second \"+\" position:", second_split)
+    
+    # Gets the start and end timestamps
+    start_timestamp = video_timestamps[:first_split]
+    end_timestamp = video_timestamps[first_split+1:second_split]
+    
+    print("Start:", start_timestamp)
+    print("End:", end_timestamp)
+    
+    # ffmpeg command to trim a video based on two timestamps
     ffmpeg_cmd = "/opt/bin/ffmpeg -i {} -ss {} -to {} -c:a copy -c:v copy {}".format(video_path, start_timestamp, end_timestamp, trimmed_video_path)
     
     cmd1 = shlex.split(ffmpeg_cmd)
     subprocess.run(cmd1, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     
-    #uploads the trimmed video file to s3 bucket
-    s3.upload_file(trimmed_video_path, bucket, trimmed_video_name)
-    
-# def get_timestamps():
-#     http = urllib3.PoolManager()
-    
-#     url = "http://10.10.200.176:3000"
-#     print(url)
-#     r = http.request("GET", url)
-#     response = r.data.decode("utf-8")
-#     print(response)
-#     start_timestamp = response[response.index["start: "]+7:response.index[" end"]]
-#     end_timestamp = response[response.index["end: "]+5:]
-    
-#     return (start_timestamp, end_timestamp)
+    # Uploads the trimmed video file to s3 bucket
+    s3.upload_file(trimmed_video_path, 'trimmed-bucket', trimmed_video_name)
