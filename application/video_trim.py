1531f5e441b42b9c75c525f21c49a88e772e909e
Donald Chiu <chiudonald7@gmail.com>
2022-11-02 14:37:52 -0400

    Added ffmpeg commands to extend the first and last frame by 1 second

--- application/video_trim.py
+++ application/video_trim.py
@@ -11,6 +11,14 @@ def trim_uploaded_video(video_path, video_file_name, bucket, video_timestamps):
     trimmed_video_name = "trimmed_{}.mp4".format(video_file_name[:-4])
     trimmed_video_path = "/tmp/{}".format(trimmed_video_name)
     
+    # Creates video name and path for the extended version
+    extend_trimmed_video_name = "first_{}".format(trimmed_video_name)
+    extend_trimmed_video_path = "/tmp/{}".format(extend_trimmed_video_name)
+    
+    # Creates video name and path for the output version
+    output_video_name = "output_{}".format(trimmed_video_name)
+    output_video_path = "/tmp/{}".format(output_video_name)
+    
     # Finds the indexes where the timestamps are split
     first_split = video_timestamps.index("+")
     second_split = video_timestamps.index("+", first_split+1)
@@ -26,10 +34,22 @@ def trim_uploaded_video(video_path, video_file_name, bucket, video_timestamps):
     print("End:", end_timestamp)
     
     # ffmpeg command to trim a video based on two timestamps
-    ffmpeg_cmd = "/opt/bin/ffmpeg -ss {} -to {} -i {} -c:a copy -c:v copy {}".format(start_timestamp, end_timestamp, video_path, trimmed_video_path)
+    ffmpeg_cmd1 = "/opt/bin/ffmpeg -ss {} -to {} -i {} -c:v copy -c:a copy {}".format(start_timestamp, end_timestamp, video_path, trimmed_video_path)
     
-    cmd1 = shlex.split(ffmpeg_cmd)
+    ## ffmpeg command to extend the first and last frames of the trimmed video by one second
+    ffmpeg_cmd2 = "/opt/bin/ffmpeg -itsoffset 1 -i {} -c copy {}".format(trimmed_video_path, extend_trimmed_video_path)
+    ffmpeg_cmd3 = "/opt/bin/ffmpeg -itsoffset -1 -i {} -c copy {}".format(extend_trimmed_video_path, output_video_path)
+    
+    cmd1 = shlex.split(ffmpeg_cmd1)
+    cmd2 = shlex.split(ffmpeg_cmd2)
+    cmd3 = shlex.split(ffmpeg_cmd3)
+    
     subprocess.run(cmd1, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print("cmd1 done")
+    subprocess.run(cmd2, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print("cmd2 done")
+    subprocess.run(cmd3, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print("cmd3 done")
     
     # Uploads the trimmed video file to s3 bucket
-    s3.upload_file(trimmed_video_path, 'trimmed-bucket', trimmed_video_name)
+    s3.upload_file(output_video_path, 'trimmed-bucket', output_video_name)
