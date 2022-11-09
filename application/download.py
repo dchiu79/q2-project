d48e5dd86342da84c96d2315477d0e69868877c7
Donald Chiu <chiudonald7@gmail.com>
2022-11-09 10:28:40 -0500

    Added downloading of start/end image files to tmp

--- application/download.py
+++ application/download.py
@@ -2,22 +2,38 @@
 import shlex
 import subprocess
 
-# Imports that handle other AWS functionality
+# Import for file management
+import shutil
+
 import boto3
 
 # Initialize the s3 client
 s3 = boto3.client('s3')
 
-def download_vid_to_tmp(bucket, file_name):
+def download_objects_to_tmp(bucket, file_name, start_image, end_image):
+    
+    # Creating path for video file
     tmp_video_file_path = "/tmp/{}".format(file_name)
-    s3_source_signed_url = s3.generate_presigned_url('get_object', Params={'Bucket':bucket, 'Key':file_name}, ExpiresIn=120)
+    s3_video_signed_url = s3.generate_presigned_url('get_object', Params={'Bucket':bucket, 'Key':file_name}, ExpiresIn=120)
     
-    print("Temporary video path:", tmp_video_file_path)
+    # Creating path for start image file
+    tmp_start_image_path = "/tmp/{}".format(start_image)
+    s3_start_signed_url = s3.generate_presigned_url('get_object', Params={'Bucket':bucket, 'Key':start_image}, ExpiresIn=120)
     
-    # ffmpeg command to copy the file to temporary directory 
-    ffmpeg_cmd = "/opt/bin/ffmpeg -i {} -c:v copy -c:a copy {}".format(s3_source_signed_url, tmp_video_file_path)
+    # Creating path for end image file
+    tmp_end_image_path = "/tmp/{}".format(end_image)
+    s3_end_signed_url = s3.generate_presigned_url('get_object', Params={'Bucket':bucket, 'Key':end_image}, ExpiresIn=120)
     
-    cmd1 = shlex.split(ffmpeg_cmd)
+    # ffmpeg command to copy the video to the temporary directory 
+    ffmpeg_cmd1 = "/opt/bin/ffmpeg -i {} -c:v copy -c:a copy {}".format(s3_video_signed_url, tmp_video_file_path)
+    
+    cmd1 = shlex.split(ffmpeg_cmd1)
     subprocess.run(cmd1, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print("Video copied to temporary directory")
     
-    return tmp_video_file_path
+    # Copying the start and end images to temporary directory
+    shutil.copy(s3_start_signed_url, tmp_start_image_path)
+    shutil.copy(s3_end_signed_url, tmp_end_image_path)
+    print("Start/End images copied to temporary directory")
+    
+    return tmp_video_file_path, tmp_start_image_path, tmp_end_image_path
