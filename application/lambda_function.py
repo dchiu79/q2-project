4475f74a3f2d4ece19929f83ae2e80699b0ff0e5
Donald Chiu <chiudonald7@gmail.com>
2022-11-23 09:30:29 -0500

    Removed unnecessary code

--- application/lambda_function.py
+++ application/lambda_function.py
@@ -15,25 +15,21 @@ from upload_to_s3 import upload_video_to_s3
 print('Loading function')
 
 def lambda_handler(event, context):
-    print("Received event: " + json.dumps(event, indent=2))
-    
+
     clear_tmp_files()
 
     # Get the object and object's key from the event
     bucket = event['Records'][0]['s3']['bucket']['name']
     video_file_name = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     
-    # Get the start and end images
-    start_image = urllib.parse.unquote_plus(event['Records'][0]['s3']['objectTwo']['key'], encoding='utf-8')
-    end_image = urllib.parse.unquote_plus(event['Records'][0]['s3']['objectThree']['key'], encoding='utf-8')
-    
     # Make the file name suitable for extracting timestamps in the right format
     video_timestamps = video_file_name.replace("-", ":")
 
+    print("Bucket:", bucket)
     print("Video file name:", video_file_name)
     print("Changed file format:", video_timestamps)
 
-    video_path, start_image_path, end_image_path = download_objects_to_tmp(bucket, video_file_name, start_image, end_image)
+    video_path, start_image_path, end_image_path = download_objects_to_tmp(bucket, video_file_name)
     
     trimmed_video_path = trim_video(video_path, video_file_name, video_timestamps)
     
@@ -51,4 +47,5 @@ def lambda_handler(event, context):
 def clear_tmp_files():
     tmp_files = os.listdir("../tmp")
     for file_name in tmp_files:
+        print(file_name)
         os.remove("../tmp/" + file_name)
--- application/video_trim.py
+++ application/video_trim.py
@@ -10,13 +10,15 @@ def trim_video(video_path, video_file_name, video_timestamps):
     # Creates video name and path for a trimmed version
     trimmed_video_name = "trimmed_{}.mp4".format(video_file_name[:-4])
     trimmed_video_path = "/tmp/{}".format(trimmed_video_name)
+    print("Trimmed video name:", trimmed_video_name)
+    print("Trimmed vide path:", trimmed_video_path)
     
     # Finds the indexes where the timestamps are split
-    first_split = video_timestamps.index("+")
-    second_split = video_timestamps.index("+", first_split+1)
+    first_split = video_timestamps.index("_")
+    second_split = video_timestamps.index("_", first_split+1)
     
-    print("First \"+\" position:", first_split)
-    print("Second \"+\" position:", second_split)
+    print("First \"_\" position:", first_split)
+    print("Second \"_\" position:", second_split)
     
     # Gets the start and end timestamps
     start_timestamp = video_timestamps[:first_split]
@@ -26,8 +28,8 @@ def trim_video(video_path, video_file_name, video_timestamps):
     print("End:", end_timestamp)
     
     # ffmpeg command to trim a video based on two timestamps
-    ffmpeg_cmd1 = "/opt/bin/ffmpeg -ss {} -to {} -i {} -c:v copy -c:a copy {}".format(start_timestamp, end_timestamp, video_path, trimmed_video_path)
-    
+    ffmpeg_cmd1 = "/opt/ffmpeglib/ffmpeg -y -ss {} -to {} -i {} -c:v copy -c:a copy {}".format(start_timestamp, end_timestamp, video_path, trimmed_video_path)
+
     # Makes the command executable and runs it
     cmd1 = shlex.split(ffmpeg_cmd1)
     subprocess.run(cmd1, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
