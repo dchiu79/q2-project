91c159ff23ed7fc3e36f957872c8f82c8338cbee
Donald Chiu <chiudonald7@gmail.com>
2022-11-30 15:38:21 -0500

    Removed python file

--- application/upload_video.py
+++
@@ -1,9 +0,0 @@
-# from google.oauth2 import service_account
-
-# SCOPES = ["https://www.googleapis.com/auth/sqlservice.admin"]
-# SERVICE_ACCOUNT_FILE = "C:\Users\chiud\Downloads\bright-link-365519-83eeb4557e87.json"
-
-# credentials = service.account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-
-def upload_video_to_youtube(video_path, video_file_name, bucket):
-    
